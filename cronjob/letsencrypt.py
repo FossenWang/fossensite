bbd5be3a8551ca12c9e8e1ed4436b3f42a217da4
FossenWang <31503932+FossenWang@users.noreply.github.com>
2018-03-20 21:28:25 +0800

    35
    fix search page bug

--- cronjob/letsencrypt.py
+++ cronjob/letsencrypt.py
@@ -6,18 +6,20 @@ from cronjob import prepare, setup_django, run, format_results, send_email
 def main():
     time.sleep(random.random() * 3600)
     prepare()
-    result = run('/usr/fossen/website/certbot-auto renew')
-    if 'Cert not yet due for renewal' in result.stderr:
+    results = []
+    results.append(run('/usr/fossen/website/certbot-auto --no-self-upgrade renew'))
+    if 'Cert not yet due for renewal' in results[-1].stderr:
         return 0
 
     try:
-        result.check_returncode()
+        for r in results:
+            r.check_returncode()
     except Exception:
         message = '续期SSL证书时发生了错误！\n\n' + traceback.format_exc() + '\n'
     else:
         message = 'SSL证书已续期\n'
 
     setup_django()
-    send_email('www.fossen.cn | 续期SSL证书', message + format_results([result]))
+    send_email('www.fossen.cn | 续期SSL证书', message + format_results(results))
 
 main()
